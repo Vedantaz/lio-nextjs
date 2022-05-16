fb69b516b46257775bc5f5160e6dd784e36ed6ad
Vedantaz <muleyvedant4@gmail.com>
2022-05-16 07:46:41 +0530

    created next app and started coding

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>Victor World <Component {...pageProps} /> </>
 }
 
 export default MyApp
